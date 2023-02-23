fd333109362f14f588e28cb3a52524cff042414a
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-02-22 21:54:36 -0500

    actualizacion de foto

--- src/Exercise3.jsx
+++ src/Exercise3.jsx
@@ -37,7 +37,7 @@ const Exercise3 = () => {
         false
       )}
       <Button variant="outline-primary" onClick={callingApi}>
-        Exercise3
+        Pintar Pato
       </Button>
     </div>
   );
